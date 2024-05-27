e7292a8b7751e6be249e7731594f3983e4108c80
TurnipWu <turnip1128@gmail.com>
2024-05-28 00:23:42 +0800

    new function

---
+++ R/PhyloCopilot.R
@@ -0,0 +1,15 @@
+#' @title Build a APG IV-based phylotree
+#' @description Constructing a phylogenetic tree based on the APG IV classification system and the megatree backbone.
+#'
+#' @param PlantList The first column of the dataframe, or the entire vector, consists of standard Latin names recognized by the World Plants website.
+#'
+#' @return A phylogenetic tree Constructed by U.PhyloMaker package.
+#' @export
+
+get_phylo <- function(PlantList) {
+  if (!requireNamespace("U.PhyloMaker",quietly = T)){stop("Please install the U.PhyloMaker package first.")}
+  data("Megatree")
+  data("APG_taxonomy")
+  result <- U.PhyloMaker::phylo.maker(get_taxon(sp.list), Megatree, APG_taxonomy, nodes.type = 1, scenario = 3)
+  return(result)
+}
