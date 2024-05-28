d8567be84941554aa91f64e290ac915c42df3146
TurnipWu <turnip1128@gmail.com>
2024-05-28 09:40:43 +0800

    Update PhyloPloter.R

--- R/PhyloPloter.R
+++ R/PhyloPloter.R
@@ -8,17 +8,15 @@
 
 
 plot_phylo <- function(PhyloTree,NodeColor=c("#f5c88f","#b3c384","#73a66b")){
-  if (class(Phylotree)[1]!="phylo"){stop("Please input the phylogenetic tree constructed by the U.phylomaker package.")}
+  if (class(Phylotree[[1]])!="phylo"){stop("Please input the phylogenetic tree constructed by the U.phylomaker package.")}
   if (!requireNamespace("tidyverse",quietly = T)){stop("Please install the tidyverse package first.")}
   if (!requireNamespace("ggtree",quietly = T)){stop("Please install the ggtree package first.")}
 
-  PhyloData <- ggplot2::fortify(PhyloTree[[1]])|>
-    dplyr::mutate(label=gsub("_"," ",label))|>
-    dplyr::left_join(PhyloTree[[2]][,c(1,5)],by=c("label"="species"))|>
-    dplyr::mutate(output.note=dplyr::case_when(is.na(output.note)~"present in megatree",T~output.note))|>
-    dplyr::rename("NodeType"=output.note)
-
-  TreeRes <- ggtree::ggtree(PhyloData,
+  TreeRes <- ggtree::ggtree(ggplot2::fortify(PhyloTree[[1]])|>
+                              dplyr::mutate(label=gsub("_"," ",label))|>
+                              dplyr::left_join(PhyloTree[[2]][,c(1,5)],by=c("label"="species"))|>
+                              dplyr::mutate(output.note=dplyr::case_when(is.na(output.note)~"present in megatree",T~output.note))|>
+                              dplyr::rename("NodeType"=output.note),
                             layout = "circular",
                             branch.length = "none",color=NA,
                             linetype=1,size=2,ladderize = T,
