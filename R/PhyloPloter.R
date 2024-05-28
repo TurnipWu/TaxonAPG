8f6565526816eee9f6343e5973ee54264abc56c4
TurnipWu <turnip1128@gmail.com>
2024-05-28 09:12:06 +0800

    little update

--- R/PhyloPloter.R
+++ R/PhyloPloter.R
@@ -7,7 +7,7 @@
 #' @export
 
 
-plot_phylo <- function(PhyloTree,NoteColor=c("#f5c88f","#b3c384","#73a66b")){
+plot_phylo <- function(PhyloTree,NodeColor=c("#f5c88f","#b3c384","#73a66b")){
   if (class(Phylotree)[1]!="phylo"){stop("Please input the phylogenetic tree constructed by the U.phylomaker package.")}
   if (!requireNamespace("tidyverse",quietly = T)){stop("Please install the tidyverse package first.")}
   if (!requireNamespace("ggtree",quietly = T)){stop("Please install the ggtree package first.")}
@@ -16,26 +16,26 @@ plot_phylo <- function(PhyloTree,NoteColor=c("#f5c88f","#b3c384","#73a66b")){
     dplyr::mutate(label=gsub("_"," ",label))|>
     dplyr::left_join(PhyloTree[[2]][,c(1,5)],by=c("label"="species"))|>
     dplyr::mutate(output.note=dplyr::case_when(is.na(output.note)~"present in megatree",T~output.note))|>
-    dplyr::rename("NoteType"=output.note)
+    dplyr::rename("NodeType"=output.note)
 
   TreeRes <- ggtree::ggtree(PhyloData,
                             layout = "circular",
                             branch.length = "none",color=NA,
                             linetype=1,size=2,ladderize = T,
                             show.legend=F)+
-    ggtree::geom_tree(ggplot2::aes(color=NoteType),
+    ggtree::geom_tree(ggplot2::aes(color=NodeType),
                       linetype=1,size=2,alpha=0.75)+
-    ggtree::geom_tiplab(ggplot2::aes(color=NoteType,label=label),
+    ggtree::geom_tiplab(ggplot2::aes(color=NodeType,label=label),
                         color="#484848",
                         fontface="italic",
                         offset = 0.05,
                         hjust=-0.14,
                         show.legend=F,
                         size=5)+
-    ggtree::geom_tippoint(ggplot2::aes(color=NoteType,shape=NoteType),
+    ggtree::geom_tippoint(ggplot2::aes(color=NodeType,shape=NodeType),
                           size=4)+
     ggplot2::xlim(-50,NA)+
-    ggplot2::scale_color_manual(values=NoteColor)+
+    ggplot2::scale_color_manual(values=NodeColor)+
     ggplot2::theme(plot.margin=ggplot2::unit(rep(5,5),'cm'),
                    legend.position = c(0.5,0.5))
   return(TreeRes)
